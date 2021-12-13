9c24d49de646d6769f51fae810082359961932e3
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-12-13 16:17:05 -0400

    Boa prática nos atributos de classe de teste

--- Formacao Testes automatizados no Android/01. Iniciando com os testes automatizados/Testes automatizados e TDD 1/Leilao/app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
+++ Formacao Testes automatizados no Android/01. Iniciando com os testes automatizados/Testes automatizados e TDD 1/Leilao/app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
@@ -5,10 +5,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class LeilaoTest {
-    private Leilao console = new Leilao("Console");
-    private Usuario alex = new Usuario("Alex");
-    private Usuario fran = new Usuario("Fran");
 
+    private final Leilao CONSOLE = new Leilao("Console");
+    private final Usuario ALEX = new Usuario("Alex");
 
     //TEMPLATE PARA NOME DO TEST
     // [nome do método] [Estado de Teste] [resultado esperado]
@@ -17,7 +16,7 @@ public class LeilaoTest {
     @Test
     public void deve_DevolverDescricao_QuandoRecebeDescricao() {
         // executar ação esperada
-        String descricaoDevolvida = console.getDescricao();
+        String descricaoDevolvida = CONSOLE.getDescricao();
 
         // testar resultado esperado
         assertEquals("Console", descricaoDevolvida);
@@ -25,48 +24,48 @@ public class LeilaoTest {
 
     @Test
     public void deve_DevolveMaiorLance_QuandoRecebeApenasUmLance() {
-        console.propoe(new Lance(alex, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 200.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente() {
-        console.propoe(new Lance(alex, 300.0));
-        console.propoe(new Lance(fran, 400.0));
+        CONSOLE.propoe(new Lance(ALEX, 300.0));
+        CONSOLE.propoe(new Lance(new Usuario("Fran"), 400.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(400.0, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolveMaiorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente() {
-        console.propoe(new Lance(alex, 15000.0));
-        console.propoe(new Lance(fran, 10000.0));
+        CONSOLE.propoe(new Lance(ALEX, 15000.0));
+        CONSOLE.propoe(new Lance(new Usuario("Fran"), 10000.0));
 
-        double maiorLanceDevolvido = console.getMaiorLance();
+        double maiorLanceDevolvido = CONSOLE.getMaiorLance();
 
         assertEquals(15000, maiorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeApenasUmLance() {
-        console.propoe(new Lance(alex, 100.0));
+        CONSOLE.propoe(new Lance(ALEX, 100.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(100.0, menorLanceDevolvido, 0.0001);
     }
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemCrescente() {
-        console.propoe(new Lance(alex, 100.0));
-        console.propoe(new Lance(fran, 200.0));
+        CONSOLE.propoe(new Lance(ALEX, 100.0));
+        CONSOLE.propoe(new Lance(new Usuario("Fran"), 200.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(100.0, menorLanceDevolvido, 0.0001);
 
@@ -74,10 +73,10 @@ public class LeilaoTest {
 
     @Test
     public void deve_DevolverMenorLance_QuandoRecebeMaisDeUmLanceEmOrdemDecrescente() {
-        console.propoe(new Lance(alex, 5000.0));
-        console.propoe(new Lance(fran, 4000.0));
+        CONSOLE.propoe(new Lance(ALEX, 5000.0));
+        CONSOLE.propoe(new Lance(new Usuario("Fran"), 4000.0));
 
-        double menorLanceDevolvido = console.getMenorLance();
+        double menorLanceDevolvido = CONSOLE.getMenorLance();
 
         assertEquals(4000.0, menorLanceDevolvido, 0.0001);
 
