f93de2db0eb1218521786b916395a2d944e0b623
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-12-14 15:51:49 -0400

    Implementando mais testes de unidade

--- Formacao Testes automatizados no Android/01. Iniciando com os testes automatizados/Testes automatizados e TDD 1/Leilao/app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
+++ Formacao Testes automatizados no Android/01. Iniciando com os testes automatizados/Testes automatizados e TDD 1/Leilao/app/src/test/java/br/com/msmlabs/tdd_leilao/model/LeilaoTest.java
@@ -101,4 +101,39 @@ public class LeilaoTest {
 
     }
 
+    @Test
+    public void deve_DevolverTresMaioresLances_QuandoNaoRecebeLances(){
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
+
+        assertEquals(0, tresMaioresLancesDevolvidos.size());
+    }
+
+    @Test
+    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasUmLance(){
+        CONSOLE.propoe(new Lance(ALEX, 200.0));
+
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
+
+        assertEquals(1, tresMaioresLancesDevolvidos.size());
+        assertEquals(200.0, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
+
+    }
+
+    @Test
+    public void deve_DevolverTresMaioresLances_QuandoRecebeApenasDoisLances(){
+        CONSOLE.propoe(new Lance(ALEX, 300.0));
+        CONSOLE.propoe(new Lance(new Usuario("Fran"), 400.0));
+
+        List<Lance> tresMaioresLancesDevolvidos = CONSOLE.tresMaioresLances();
+
+        assertEquals(2, tresMaioresLancesDevolvidos.size());
+
+        assertEquals(400.0, tresMaioresLancesDevolvidos.get(0).getValor(), DELTA);
+        assertEquals(300.0, tresMaioresLancesDevolvidos.get(1).getValor(), DELTA);
+        
+
+
+    }
+
+
 }
