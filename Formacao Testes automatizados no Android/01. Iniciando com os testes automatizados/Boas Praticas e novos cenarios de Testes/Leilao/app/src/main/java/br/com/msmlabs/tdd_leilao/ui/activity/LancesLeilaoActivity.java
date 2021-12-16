5e7897781fa35864a607dc8e4119bec06ffe6e2f
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-12-16 16:40:02 -0400

    Boas Práticas e novos cenários de testes

---
+++ Formacao Testes automatizados no Android/01. Iniciando com os testes automatizados/Boas Praticas e novos cenarios de Testes/Leilao/app/src/main/java/br/com/msmlabs/tdd_leilao/ui/activity/LancesLeilaoActivity.java
@@ -0,0 +1,39 @@
+package br.com.msmlabs.tdd_leilao.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.msmlabs.tdd_leilao.R;
+import br.com.msmlabs.tdd_leilao.model.Lance;
+import br.com.msmlabs.tdd_leilao.model.Leilao;
+
+public class LancesLeilaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lances_leilao);
+
+        Intent dadosRecebidos = getIntent();
+        if (dadosRecebidos.hasExtra("leilao")) {
+            Leilao leilao = (Leilao) dadosRecebidos.getSerializableExtra("leilao");
+            TextView descricao = findViewById(R.id.lances_leilao_descricao);
+            descricao.setText(leilao.getDescricao());
+            TextView maiorLance = findViewById(R.id.lances_leilao_maior_lance);
+            maiorLance.setText(String.valueOf(leilao.getMaiorLance()));
+            TextView menorLance = findViewById(R.id.lances_leilao_menor_lance);
+            menorLance.setText(String.valueOf(leilao.getMenorLance()));
+            TextView maioresLances = findViewById(R.id.lances_leilao_maiores_lances);
+            StringBuilder sb = new StringBuilder();
+            for (Lance lance :
+                    leilao.tresMaioresLances()) {
+                sb.append(lance.getValor() + "\n");
+            }
+            String maioresLancesEmTexto = sb.toString();
+            maioresLances.setText(maioresLancesEmTexto);
+        }
+    }
+}
